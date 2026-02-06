e5df486dbc030ffa051243dec08132be093576f5
최상현 <itchoi0429@gmail.com>
2026-02-06 18:57:49 +0900

    init commit

---
+++ src/test/kotlin/org/example/studycafe/StudyCafeApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.studycafe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyCafeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
